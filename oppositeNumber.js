b0e644f7de7e8b55f28f54f538162ac7eb3f29ad
kathrynspeltz <kathrynspeltz@gmail.com>
2024-02-20 18:49:55 -0600

    numbers practice

---
+++ oppositeNumber.js
@@ -0,0 +1,9 @@
+/* Very simple, given a number (integer / decimal / both depending on the language), find its opposite */
+
+function opposite(number) {
+    return -number
+}
+
+console.log(opposite(1))
+console.log(opposite(-12))
+console.log(opposite(6))
