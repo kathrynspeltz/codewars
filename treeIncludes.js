5114f2c820d1f1b3e9a3856408bf54a236489dab
kathrynspeltz <kathrynspeltz@gmail.com>
2024-03-11 20:25:28 -0500

    tree practice

---
+++ treeIncludes.js
@@ -0,0 +1,45 @@
+/* respond with true or false if the tree contains a given value */
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const treeIncludes = (root, target) => {
+    if (root == null) return false
+    const queue = [root];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        if (current.val == target) {
+            return true
+        }
+
+        if (current.left) queue.push(current.left)
+        if (current.right) queue.push(current.right)
+    }
+    return false
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//     / \
+//    b    c
+//   / \    \
+//  d   e    f
+
+console.log(breadthFirstValues(a))
